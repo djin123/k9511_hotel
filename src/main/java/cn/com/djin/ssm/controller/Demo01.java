b6846b32fb430161992cf5658b6348b2c65f5bb6
djin123 <502037675@qq.com>
2020-05-12 11:09:50 +0800

    组员A第一次修改Demo01的代码，创建test1方法。。

--- src/main/java/cn/com/djin/ssm/controller/Demo01.java
+++ src/main/java/cn/com/djin/ssm/controller/Demo01.java
@@ -5,4 +5,9 @@ package cn.com.djin.ssm.controller;
  * date:2020-05-12-10:00
  */
 public class Demo01 {
+
+    public void test1(){
+        System.out.println("组员A第一次修改Demo01的代码，创建test1方法。。");
+    }
+
 }
